b42cff899d976beae23beac4db2ffdfe42b6c39e
Eduardo Suarez <eduardosuarez@gmail.com>
2019-03-25 19:23:32 +0100

    Add SpringSecurity basic config and some hardcoded users.

---
+++ src/main/java/info/esuarez/springsecuritydemo/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package info.esuarez.springsecuritydemo.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("test123").roles("MANAGER"))
+                .withUser(users.username("susan").password("test1234").roles("ADMIN"));
+    }
+}
